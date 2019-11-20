8af8144b224ab29f907cffb8963f48a8966b3435
Ryan Reyes <ryan.david.reyes@gmail.com>
2019-11-20 10:00:46 -0500

    Updated stimulation patterns.

--- P00_Template.cydsn/BAL06_gait.h
+++ P00_Template.cydsn/BAL06_gait.h
@@ -3,16 +3,19 @@
 // Version 1.0
 // Repo: https://github.com/lilulab/CwruStim_beta
 
+#ifndef BAL_06_H_
+#define BAL_06_H_
 #include <stdint.h>
+
 
 // ------ Walk ------
 
-// Step Duration
-extern  float gait_walk_R_duration;
-extern  float gait_walk_L_duration;
+// Step Duration 
+extern float gait_walk_R_duration; 
+extern float gait_walk_L_duration; 
 
-// Channel Amplitude
-extern  uint8_t gait_B1_AMP[16];
+// Channel Amplitude 
+extern uint8_t gait_B1_AMP[16];
 
 
 // Board_IST16------------------
@@ -20,31 +23,35 @@ extern  uint8_t gait_B1_AMP[16];
 // Left Step ---------
 
 // Percent Pattern
-extern  uint16_t gait_walk_B1_L_PP[16][8];
+extern uint16_t gait_walk_B1_L_PP[16][8];
+
 
 // Pulse Width
-extern  uint8_t gait_walk_B1_L_PW[16][8];
+extern uint8_t gait_walk_B1_L_PW[16][8];
+
 
 // Inter Phase Interval
-extern  uint8_t gait_walk_B1_L_IPI[16];
+extern uint8_t gait_walk_B1_L_IPI[16];
 
 
 // Right Step ---------
 
 // Percent Pattern
-extern  uint16_t gait_walk_B1_R_PP[16][8];
+extern uint16_t gait_walk_B1_R_PP[16][8] ;
+
 
 // Pulse Width
-extern  uint8_t gait_walk_B1_R_PW[16][8];
+extern uint8_t gait_walk_B1_R_PW[16][8] ;
+
 
 // Inter Phase Interval
-extern  uint8_t gait_walk_B1_R_IPI[16];
+extern uint8_t gait_walk_B1_R_IPI[16];
 
 
 // ------ Stand ------
 
-// Step Duration
-extern  float gait_stand_duration;
+// Step Duration 
+extern float gait_stand_duration; 
 
 
 // Board_IST16------------------
@@ -52,19 +59,21 @@ extern  float gait_stand_duration;
 // Step ---------
 
 // Percent Pattern
-extern  uint16_t gait_stand_B1_PP[16][8];
+extern uint16_t gait_stand_B1_PP[16][8] ;
+
 
 // Pulse Width
-extern  uint8_t gait_stand_B1_PW[16][8];
+extern uint8_t gait_stand_B1_PW[16][8] ;
+
 
 // Inter Phase Interval
-extern  uint8_t gait_stand_B1_IPI[16];
+extern uint8_t gait_stand_B1_IPI[16] ;
 
 
 // ------ Sit ------
 
-// Step Duration
-extern  float gait_sit_duration ;
+// Step Duration 
+extern float gait_sit_duration; 
 
 
 // Board_IST16------------------
@@ -72,14 +81,16 @@ extern  float gait_sit_duration ;
 // Step ---------
 
 // Percent Pattern
-extern  uint16_t gait_sit_B1_PP[16][8];
+extern uint16_t gait_sit_B1_PP[16][8] ;
+
 
 // Pulse Width
-extern  uint8_t gait_sit_B1_PW[16][8];
+extern uint8_t gait_sit_B1_PW[16][8] ;
 
 
 // Inter Phase Interval
-extern  uint8_t gait_sit_B1_IPI[16];
+extern uint8_t gait_sit_B1_IPI[16] ;
 
 // File End
+#endif
 
